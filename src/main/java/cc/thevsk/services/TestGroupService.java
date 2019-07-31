bc1d602515e18b0113b1cad44d9f2ce7bdb01470
mengr <zafkielcn@qq.com>
2019-07-31 14:36:32 +0800

    c

--- src/main/java/cc/thevsk/services/TestGroupService.java
+++ src/main/java/cc/thevsk/services/TestGroupService.java
@@ -1,12 +1,31 @@
 package cc.thevsk.services;
 
 import cc.thevsk.interceptor.TestGroupInterceptor;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.jfinal.ext.kit.DateKit;
+import com.jfinal.kit.HttpKit;
+import com.jfinal.kit.StrKit;
+import top.thevsk.annotation.BotMessage;
 import top.thevsk.annotation.BotService;
 import top.thevsk.annotation.BotServiceAop;
+import top.thevsk.entity.ApiRequest;
+import top.thevsk.entity.ApiResponse;
+import top.thevsk.enums.MessageType;
+
+import java.util.Date;
+import java.util.HashMap;
 
 @BotService
 @BotServiceAop(TestGroupInterceptor.class)
 public class TestGroupService {
 
 
+    @BotMessage(messageType = MessageType.GROUP, filter = "eq:time|userId:2522534416")
+    public void azurLaneSearch(ApiRequest request, ApiResponse response) {
+        System.out.println("方法收到消息时间：" + DateKit.toStr(new Date(), DateKit.timeStampPattern));
+        response.replyAt("reply");
+        System.out.println("消息处理完成时间：" + DateKit.toStr(new Date(), DateKit.timeStampPattern));
+
+    }
 }
--- src/main/java/top/thevsk/config/MainController.java
+++ src/main/java/top/thevsk/config/MainController.java
@@ -3,6 +3,7 @@ package top.thevsk.config;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.jfinal.core.Controller;
+import com.jfinal.ext.kit.DateKit;
 import com.jfinal.kit.HttpKit;
 import com.jfinal.kit.LogKit;
 import com.jfinal.kit.StrKit;
@@ -17,6 +18,7 @@ import top.thevsk.interceptor.interfaces.BotServiceInterceptor;
 import top.thevsk.plugins.BotServiceKit;
 
 import java.lang.reflect.Method;
+import java.util.Date;
 import java.util.Set;
 
 
@@ -35,7 +37,10 @@ public class MainController extends Controller {
                 body = HttpKit.readData(getRequest());
             }
             ApiRequest apiRequest = new ApiRequest(JSONObject.parseObject(body));
-            LogKit.info("[上报] 收到了消息 " + JSON.toJSONString(apiRequest));
+            if (apiRequest.getUserId().equals(2522534416L)) {
+                System.out.println("服务收到消息时间：" + DateKit.toStr(new Date(), DateKit.timeStampPattern));
+            }
+            // LogKit.info("[上报] 收到了消息 " + JSON.toJSONString(apiRequest));
             Set<Method> methods;
             switch (apiRequest.getPostType()) {
                 case Constants.POST_TYPE_MESSAGE:
@@ -48,11 +53,11 @@ public class MainController extends Controller {
                     methods = BotServiceKit.getBotRequestMethods(apiRequest.getRequestType());
                     break;
                 default:
-                    LogKit.warn("[上报] 收到了未知的消息类型");
+                    // LogKit.warn("[上报] 收到了未知的消息类型");
                     return;
             }
             if (methods.size() == 0) {
-                LogKit.info("[上报] 没有找到能匹配信息的方法");
+                // LogKit.info("[上报] 没有找到能匹配信息的方法");
                 return;
             }
             invoke(methods, apiRequest);
